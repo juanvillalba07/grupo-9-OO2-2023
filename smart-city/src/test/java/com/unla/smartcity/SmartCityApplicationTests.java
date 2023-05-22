4d611614b1bbbdd6cb17c4bb5a3e1eddd622be6c
jvillalba <jvillalba@asap-consulting.net>
2023-05-22 11:44:43 -0300

    commit inicial del proyecto

---
+++ smart-city/src/test/java/com/unla/smartcity/SmartCityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unla.smartcity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartCityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
