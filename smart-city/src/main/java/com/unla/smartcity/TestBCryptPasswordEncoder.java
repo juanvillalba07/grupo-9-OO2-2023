80f18abdc2f03e809a70ad0c741ed4e438f37f16
Strychi8 <tomas2000T@gmail.com>
2023-05-26 16:57:51 -0300

    Nuevas carpetas,archivos y clases(controller,helpers y templates)

---
+++ smart-city/src/main/java/com/unla/smartcity/TestBCryptPasswordEncoder.java
@@ -0,0 +1,17 @@
+package com.unla.smartcity;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class TestBCryptPasswordEncoder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BCryptPasswordEncoder pe = new BCryptPasswordEncoder();
+		System.out.println(pe.encode("root"));
+		
+		/*BCryptPasswordEncoder pe1 = new BCryptPasswordEncoder();
+		System.out.println(pe1.encode("crud"));*/
+		
+	}
+
+}
---
+++ smart-city/src/main/java/com/unla/smartcity/helpers/ViewRouteHelper.java
@@ -0,0 +1,14 @@
+package com.unla.smartcity.helpers;
+
+public class ViewRouteHelper {
+	
+	//USER
+	public final static String USER_LOGIN = "user/login";
+	public final static String USER_LOGOUT = "user/logout";
+	
+	//UNLa
+	public final static String INDEX = "unla/index";
+	
+	/**** Redirects ****/
+	public final static String ROUTE = "/index";
+}
